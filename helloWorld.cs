b937ddab6a7f4b6a518ea8c6a1a362eabf58ee49
Anya Hossaini <44172471+AnyaHossaini@users.noreply.github.com>
2020-06-08 15:30:23 -0400

    Create helloWorld.cs

---
+++ helloWorld.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}s
