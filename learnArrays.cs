eb514a0f17dc7f9ab94f8e09d7ebe9a3716d7f18
Anya Hossaini <44172471+AnyaHossaini@users.noreply.github.com>
2020-06-09 10:26:51 -0400

    Create learnArrays.cs
    basic arrays

---
+++ learnArrays.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.text;
+using System.Threading.Tasks;
+
+namespace learnArrays
+{
+    class Arrays
+    {
+        static void Main(string[]args)
+        {
+            int [] luckyNumbers = {4,8,15,16,23,42};
+            Console.WriteLine(luckyNumbers[0]);
+            string [] friends = new string[3]; // This tells how many elements I want the array to hold
+            friends[0] = "Ara";
+            friends[1] = "Asiyah";
+        }
+    }
+}
