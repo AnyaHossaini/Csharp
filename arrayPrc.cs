ec1c9689aa93631c994d250fe2a4149d545c229f
Anya Hossaini <44172471+AnyaHossaini@users.noreply.github.com>
2020-06-10 16:43:52 -0400

    Create arrayPrc.cs

---
+++ arrayPrc.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arrays 
+{
+    class arrayPrc 
+    {
+        static void Main(string[]args)
+        {
+            var numbers = new int[3];
+            numbers[0]=1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+        }
+    }
+}
